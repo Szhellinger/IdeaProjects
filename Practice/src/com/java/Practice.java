986e89c54e41dc80c7c01e86222534cc15460127
Szhell Zou <szhellinger@gmail.com>
2017-05-21 17:44:47 +0800

    initial project version

---
+++ Practice/src/com/java/Practice.java
@@ -0,0 +1,12 @@
+package com.java;
+
+import java.util.*;
+
+/**
+ * Created by szhell on 5/21/17.
+ */
+public class Practice {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
